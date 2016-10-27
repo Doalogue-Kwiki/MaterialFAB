6a90340d4d55d8ca31995799b073a3e516bd37ef
wottem <yotam.n.h@gmail.com>
2016-10-27 12:01:04 +0000

    wip

--- MaterialFAB.php
+++ MaterialFAB.php
@@ -7,10 +7,10 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	
 	$wgExtensionMessagesFiles['MaterialFABMagic'] = __DIR__ . '/MaterialFAB.i18n.magic.php';
 	wfWarn(
-		'Deprecated PHP entry point used for Accessibility extension. Please use wfLoadExtension ' .
+		'Deprecated PHP entry point used for MaterialFAB extension. Please use wfLoadExtension ' .
 		'instead, see https://www.mediawiki.org/wiki/Extension_registration for more details.'
 	);
 	return true;
 } else {
-	die( 'This version of the Accessibility extension requires MediaWiki 1.25+' );
+	die( 'This version of the MaterialFAB extension requires MediaWiki 1.25+' );
 }
